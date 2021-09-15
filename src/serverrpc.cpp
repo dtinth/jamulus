0aa7b63ee7d95cfcf54afc6244746e979bd2e7d0
dtinth on MBP M1 <dtinth@spacet.me>
2021-09-15 18:19:25 +0700

    [json-rpc-docs] Add documentation for JSON-RPC

--- src/serverrpc.cpp
+++ src/serverrpc.cpp
@@ -26,11 +26,30 @@
 
 CServerRpc::CServerRpc ( QObject* parent, CServer* pServer, CRpcServer* pRpcServer ) : QObject ( parent )
 {
+    // Already documented in CClientRpc.
     pRpcServer->HandleMethod ( "jamulus/getMode", [=] ( const QJsonObject& params, QJsonObject& response ) {
         QJsonObject result{ { "mode", "server" } };
         response["result"] = result;
     } );
 
+    /// @rpc_method jamulusserver/getServerStatus
+    /// @brief Returns the server status, including the list of connected clients and the recorder state.
+    /// @param {object} params - No parameters (empty object).
+    /// @result {string} result.name - The server name.
+    /// @result {string} result.city - The server city.
+    /// @result {number} result.countryId - The server country ID (see QLocale::Country).
+    /// @result {string} result.welcomeMessage - The server welcome message.
+    /// @result {string} result.registrationStatus - The server registration status (see ESvrRegStatus).
+    /// @result {array} result.clients - The list of connected clients.
+    /// @result {string} result.clients[*].address - The client’s address (ip:port).
+    /// @result {string} result.clients[*].name - The client’s name.
+    /// @result {number} result.clients[*].jitterBufferSize - The client’s jitter buffer size.
+    /// @result {number} result.clients[*].channels - The number of audio channels of the client.
+    /// @result {object} result.recorder - The recorder state.
+    /// @result {boolean} result.recorder.initialised - True if the recorder is initialised.
+    /// @result {string} result.recorder.errorMessage - The recorder error message, if any.
+    /// @result {boolean} result.recorder.enabled - True if the recorder is enabled.
+    /// @result {string} result.recorder.recordingDirectory - The recorder recording directory.
     pRpcServer->HandleMethod ( "jamulusserver/getServerStatus", [=] ( const QJsonObject& params, QJsonObject& response ) {
         QJsonArray            clients;
         CVector<CHostAddress> vecHostAddresses;
@@ -79,6 +98,10 @@ CServerRpc::CServerRpc ( QObject* parent, CServer* pServer, CRpcServer* pRpcServ
         response["result"] = result;
     } );
 
+    /// @rpc_method jamulusserver/setServerName
+    /// @brief Sets the server name.
+    /// @param {string} params.serverName - The new server name.
+    /// @result {string} result - Always "ok".
     pRpcServer->HandleMethod ( "jamulusserver/setServerName", [=] ( const QJsonObject& params, QJsonObject& response ) {
         auto jsonServerName = params["serverName"];
         if ( !jsonServerName.isString() )
@@ -91,6 +114,10 @@ CServerRpc::CServerRpc ( QObject* parent, CServer* pServer, CRpcServer* pRpcServ
         response["result"] = "ok";
     } );
 
+    /// @rpc_method jamulusserver/setWelcomeMessage
+    /// @brief Sets the server welcome message.
+    /// @param {string} params.welcomeMessage - The new welcome message.
+    /// @result {string} result - Always "ok".
     pRpcServer->HandleMethod ( "jamulusserver/setWelcomeMessage", [=] ( const QJsonObject& params, QJsonObject& response ) {
         auto jsonWelcomeMessage = params["welcomeMessage"];
         if ( !jsonWelcomeMessage.isString() )
@@ -103,6 +130,11 @@ CServerRpc::CServerRpc ( QObject* parent, CServer* pServer, CRpcServer* pRpcServ
         response["result"] = "ok";
     } );
 
+    /// @rpc_method jamulusserver/setRecordingDirectory
+    /// @brief Sets the server recording directory.
+    /// @param {string} params.recordingDirectory - The new recording directory.
+    /// @result {string} result - Always "acknowledged".
+    ///  To check if the directory was changed, call `jamulusserver/getServerStatus` again.
     pRpcServer->HandleMethod ( "jamulusserver/setRecordingDirectory", [=] ( const QJsonObject& params, QJsonObject& response ) {
         auto jsonRecordingDirectory = params["recordingDirectory"];
         if ( !jsonRecordingDirectory.isString() )
@@ -115,16 +147,31 @@ CServerRpc::CServerRpc ( QObject* parent, CServer* pServer, CRpcServer* pRpcServ
         response["result"] = "acknowledged";
     } );
 
+    /// @rpc_method jamulusserver/startRecording
+    /// @brief Starts the server recording.
+    /// @param {object} params - No parameters (empty object).
+    /// @result {string} result - Always "acknowledged".
+    ///  To check if the recording was enabled, call `jamulusserver/getServerStatus` again.
     pRpcServer->HandleMethod ( "jamulusserver/startRecording", [=] ( const QJsonObject& params, QJsonObject& response ) {
         pServer->SetEnableRecording ( true );
         response["result"] = "acknowledged";
     } );
 
+    /// @rpc_method jamulusserver/stopRecording
+    /// @brief Stops the server recording.
+    /// @param {object} params - No parameters (empty object).
+    /// @result {string} result - Always "acknowledged".
+    ///  To check if the recording was disabled, call `jamulusserver/getServerStatus` again.
     pRpcServer->HandleMethod ( "jamulusserver/stopRecording", [=] ( const QJsonObject& params, QJsonObject& response ) {
         pServer->SetEnableRecording ( false );
         response["result"] = "acknowledged";
     } );
 
+    /// @rpc_method jamulusserver/restartRecording
+    /// @brief Restarts the recording into a new directory.
+    /// @param {object} params - No parameters (empty object).
+    /// @result {string} result - Always "acknowledged".
+    ///  To check if the recording was restarted or if there is any error, call `jamulusserver/getServerStatus` again.
     pRpcServer->HandleMethod ( "jamulusserver/restartRecording", [=] ( const QJsonObject& params, QJsonObject& response ) {
         pServer->RequestNewRecording();
         response["result"] = "acknowledged";
